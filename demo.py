809574369e825d7cb94369e722dcbb9b8b322ffb
root <root@DESKTOP-PVCPKG9>
2023-10-17 18:12:55 +0900

    10/17

--- demo.py
+++ demo.py
@@ -4,8 +4,15 @@ import elasticdeform
 import cv2
 
 def strength(x, y, x_beg, y_beg, x_end, y_end, coef, sigma):
-    amplitude = (coef * np.exp(-((x - x_beg) ** 2 + (y - y_beg) ** 2) / (2 * sigma ** 2)) + coef * np.exp(-((x - x_end) ** 2 + (y - y_end) ** 2) / (2 * sigma ** 2)))
-    angle = np.arctan((x_beg - x_end) / (y_beg - y_end))
+    a = (y_end - y_beg) / (x_end - x_beg)
+    b = y_end - a * x_end
+    distance = np.abs(a * x - y + b) / np.sqrt(a ** 2 + 1)
+    
+    amplitude, angle = 0, 0
+    if distance < 10 and distance != 0:
+        amplitude = 20 / distance
+    #amplitude = (coef * np.exp(-((x - x_beg) ** 2 + (y - y_beg) ** 2) / (2 * sigma ** 2)) + coef * np.exp(-((x - x_end) ** 2 + (y - y_end) ** 2) / (2 * sigma ** 2)))
+        angle = np.arctan((x_beg - x) / (y_beg - y))
     return (amplitude, angle)
 
 cap = cv2.VideoCapture("/opencv/opencv/samples/data/vtest.avi")
@@ -20,9 +27,9 @@ col_nodes = cols
 displacement = np.zeros([2, row_nodes, col_nodes])
 
 x_beg = 130
-y_beg = 130
-x_end = 160
-y_end = 160
+y_beg = -1
+x_end = 131
+y_end = 1
         
         
 
@@ -43,10 +50,8 @@ while(cap.isOpened()):
     if cv2.waitKey(1) & 0xFF == ord('q'):
         break    
     
-    x_beg += 1
-    y_beg += 1
+    y_end += np.sqrt(3)
     x_end += 1
-    y_end += 1
     
 
 cap.release()    
--- test.py
+++ test.py
@@ -3,11 +3,18 @@ import matplotlib.pyplot as plt
 import elasticdeform
 
 def strength(x, y, x_beg, y_beg, x_end, y_end, coef, sigma):
-    amplitude = (coef * np.exp(-((x - x_beg) ** 2 + (y - y_beg) ** 2) / (2 * sigma ** 2)) + coef * np.exp(-((x - x_end) ** 2 + (y - y_end) ** 2) / (2 * sigma ** 2)))
-    angle = np.arctan((x_beg - x_end) / (y_beg - y_end))
+    a = (y_end - y_beg) / (x_end - x_beg)
+    b = y_end - a * x_end
+    distance = np.abs(a * x - y + b) / np.sqrt(a ** 2 + 1)
+    
+    amplitude, angle = 0, 0
+    if distance < 10 and distance != 0:
+        amplitude = 20 / distance
+    #amplitude = (coef * np.exp(-((x - x_beg) ** 2 + (y - y_beg) ** 2) / (2 * sigma ** 2)) + coef * np.exp(-((x - x_end) ** 2 + (y - y_end) ** 2) / (2 * sigma ** 2)))
+        angle = np.arctan((x_beg - x) / (y_beg - y))
     return (amplitude, angle)
 
-img = plt.imread("/opencv/opencv/samples/data/lena.jpg")
+img = plt.imread("/mnt/c/dl/frame_0.png")
 rows, cols = img.shape[0], img.shape[1]
 
 rows, cols = 256, 256
@@ -19,25 +26,23 @@ row_nodes = rows
 col_nodes = cols
 displacement = np.zeros([2, row_nodes, col_nodes])
 
-x_beg = 130
-y_beg = 130
-x_end = 160
-y_end = 160
+x_beg = 10
+y_beg = -1
+x_end = 150
+y_end = 100
 
 for i in range(row_nodes):
     for j in range(col_nodes):
-        amplitude, angle = strength(i, j, x_beg, y_beg, x_end, y_end, 20, 40)
+        amplitude, angle = strength(i, j, x_beg, y_beg, x_end, y_end, 20, 20)
         displacement[0][i][j] = -amplitude * np.cos(angle)
         displacement[1][i][j] = -amplitude * np.sin(angle)
-        
-        
 
 # perform forward deformation
 X_deformed = elasticdeform.deform_grid(X, displacement, axis = (0, 1))
 img_deformed = elasticdeform.deform_grid(img, displacement, axis = (0, 1))
 
 plt.figure()
-plt.imshow(X_deformed)
+plt.imshow(img)
 
 plt.figure()
 plt.imshow(img_deformed)
