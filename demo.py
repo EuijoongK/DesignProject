117db85fa30e9d8696f35e69ca2b3085189c4f5a
root <root@DESKTOP-PVCPKG9>
2023-10-17 17:30:06 +0900

    10/17

--- demo.py
+++ demo.py
@@ -1,7 +1,7 @@
-import cv2
+import numpy as np
 import matplotlib.pyplot as plt
 import elasticdeform
-import numpy as np
+import cv2
 
 def strength(x, y, x_beg, y_beg, x_end, y_end, coef, sigma):
     amplitude = (coef * np.exp(-((x - x_beg) ** 2 + (y - y_beg) ** 2) / (2 * sigma ** 2)) + coef * np.exp(-((x - x_end) ** 2 + (y - y_end) ** 2) / (2 * sigma ** 2)))
@@ -23,22 +23,31 @@ x_beg = 130
 y_beg = 130
 x_end = 160
 y_end = 160
+        
+        
 
-for i in range(row_nodes):
-    for j in range(col_nodes):
-        amplitude, angle = strength(i, j, x_beg, y_beg, x_end, y_end, 20, 40)
-        displacement[0][i][j] = -amplitude * np.cos(angle)
-        displacement[1][i][j] = -amplitude * np.sin(angle)
-
+# perform forward deformation
 
 while(cap.isOpened()):
     ret, frame = cap.read()
+    
+    for i in range(row_nodes):
+        for j in range(col_nodes):
+            amplitude, angle = strength(i, j, x_beg, y_beg, x_end, y_end, 20, 40)
+            displacement[0][i][j] = -amplitude * np.cos(angle)
+            displacement[1][i][j] = -amplitude * np.sin(angle)
     
     
     frame_deformed = elasticdeform.deform_grid(frame, displacement, axis = (0, 1))  
     cv2.imshow("test", frame_deformed)   
     if cv2.waitKey(1) & 0xFF == ord('q'):
         break    
+    
+    x_beg += 1
+    y_beg += 1
+    x_end += 1
+    y_end += 1
+    
 
 cap.release()    
 cv2.destroyAllWindows()
--- test.py
+++ test.py
@@ -1,33 +1,45 @@
-import numpy
+import numpy as np
 import matplotlib.pyplot as plt
 import elasticdeform
 
-X = plt.imread("/opencv/opencv/samples/data/lena.jpg")
+def strength(x, y, x_beg, y_beg, x_end, y_end, coef, sigma):
+    amplitude = (coef * np.exp(-((x - x_beg) ** 2 + (y - y_beg) ** 2) / (2 * sigma ** 2)) + coef * np.exp(-((x - x_end) ** 2 + (y - y_end) ** 2) / (2 * sigma ** 2)))
+    angle = np.arctan((x_beg - x_end) / (y_beg - y_end))
+    return (amplitude, angle)
 
-X = numpy.zeros((200, 300))
+img = plt.imread("/opencv/opencv/samples/data/lena.jpg")
+rows, cols = img.shape[0], img.shape[1]
+
+rows, cols = 256, 256
+X = np.zeros((rows, cols))
 X[::10, ::10] = 1
 
 # generate a deformation grid
-displacement = numpy.zeros([2, 2, 2])
+row_nodes = rows
+col_nodes = cols
+displacement = np.zeros([2, row_nodes, col_nodes])
 
-a = 200
+x_beg = 130
+y_beg = 130
+x_end = 160
+y_end = 160
 
-displacement[0][0][0] = 0
-displacement[1][0][0] = 0
-
-displacement[0][0][1] = 0
-displacement[1][0][1] = 0
-
-displacement[0][1][0] = 0
-displacement[1][1][0] = 0
-
-displacement[0][1][1] = 0
-displacement[1][1][1] = a
+for i in range(row_nodes):
+    for j in range(col_nodes):
+        amplitude, angle = strength(i, j, x_beg, y_beg, x_end, y_end, 20, 40)
+        displacement[0][i][j] = -amplitude * np.cos(angle)
+        displacement[1][i][j] = -amplitude * np.sin(angle)
+        
+        
 
 # perform forward deformation
 X_deformed = elasticdeform.deform_grid(X, displacement, axis = (0, 1))
+img_deformed = elasticdeform.deform_grid(img, displacement, axis = (0, 1))
 
 plt.figure()
 plt.imshow(X_deformed)
+
+plt.figure()
+plt.imshow(img_deformed)
 
 plt.show()
