024f4449425e45705f93c0cebea497fc09a82032
root <root@DESKTOP-PVCPKG9>
2023-10-23 17:10:21 +0900

    10/23

--- demo.py
+++ demo.py
@@ -3,29 +3,34 @@ import matplotlib.pyplot as plt
 import elasticdeform
 import cv2
 
-def deform_field(x, y, x_beg, y_beg, x_end, y_end, coef = 20, sigma = 20):
-    a = (y_end - y_beg) / (x_end + x_beg)
+def deform_field(x, y, x_beg, y_beg, x_end, y_end, strength = 15, coef = 20, sigma = 20):
+    a = (y_end - y_beg) / (x_end - x_beg)
     b = y_end - a * x_end
     distance = np.abs(a * x - y + b) / np.sqrt(a ** 2 + 1)
-
-    in_range = True
-    if y_end < y:
-        in_range = False
+    
     amplitude, angle = 0, 0
-    if distance < 10 and distance > 2 and in_range:
-        amplitude = 40 / (distance ** 2)
+    if y_end < y or y_beg == y:
+        return (amplitude, angle)
+    
+    if distance != 0:
+        amplitude = strength / (distance ** 2)
         angle = np.arctan((x_beg - x) / (y_beg - y))
+        
     return (amplitude, angle)
 
-img = plt.imread("/mnt/c/Users/sammy/Desktop/Bmode.png")
+img = plt.imread("/mnt/c/dl/frame_0.png")
+
+if len(img) > 2:
+    img = img.mean(axis = 2)
+    
 rows, cols = img.shape[0], img.shape[1]
 
 displacement = np.zeros([2, rows, cols])
 
-x_beg = 400
-y_beg = -1
-x_end = 600
-y_end = 350
+x_beg = 200
+y_beg = 10
+x_end = 300
+y_end = 200
 
 for i in range(rows):
     for j in range(cols):
@@ -37,6 +42,15 @@ for i in range(rows):
 img_deformed = elasticdeform.deform_grid(img, displacement,
                                          axis = (0, 1))
 
+for i in range(rows):
+    for j in range(cols):
+        if img_deformed[i][j] == 0:
+            img_deformed[i][j] = img[i][j]
+
+
 plt.figure()
-plt.imshow(img_deformed, cmap = "gray")
+plt.imshow(img, cmap = 'gray')
+
+plt.figure()
+plt.imshow(img_deformed, cmap = 'gray')
 plt.show()
--- test.py
+++ test.py
@@ -2,22 +2,25 @@ import numpy as np
 import matplotlib.pyplot as plt
 import elasticdeform
 
-def strength(x, y, x_beg, y_beg, x_end, y_end, coef, sigma):
+def deform_field(x, y, x_beg, y_beg, x_end, y_end, coef = 20, sigma = 20):
     a = (y_end - y_beg) / (x_end - x_beg)
     b = y_end - a * x_end
     distance = np.abs(a * x - y + b) / np.sqrt(a ** 2 + 1)
     
     amplitude, angle = 0, 0
-    if distance < 10 and distance != 0:
-        amplitude = 20 / distance
-    #amplitude = (coef * np.exp(-((x - x_beg) ** 2 + (y - y_beg) ** 2) / (2 * sigma ** 2)) + coef * np.exp(-((x - x_end) ** 2 + (y - y_end) ** 2) / (2 * sigma ** 2)))
+    if y_end < y or y_beg == y:
+        return (amplitude, angle)
+    
+    if distance != 0:
+        amplitude = 20 / (distance ** 2)
         angle = np.arctan((x_beg - x) / (y_beg - y))
+        
     return (amplitude, angle)
 
-img = plt.imread("/mnt/c/Users/sammy/Desktop/Bmode.jpg")
+img = plt.imread("/mnt/c/dl/frame_0.png")
 rows, cols = img.shape[0], img.shape[1]
 
-rows, cols = 256, 256
+rows, cols = 512, 512
 X = np.zeros((rows, cols))
 X[::10, ::10] = 1
 
@@ -33,7 +36,7 @@ y_end = 100
 
 for i in range(row_nodes):
     for j in range(col_nodes):
-        amplitude, angle = strength(i, j, x_beg, y_beg, x_end, y_end, 20, 20)
+        amplitude, angle = deform_field(i, j, x_beg, y_beg, x_end, y_end, 20, 20)
         displacement[0][i][j] = -amplitude * np.cos(angle)
         displacement[1][i][j] = -amplitude * np.sin(angle)
 
@@ -42,9 +45,9 @@ X_deformed = elasticdeform.deform_grid(X, displacement, axis = (0, 1))
 img_deformed = elasticdeform.deform_grid(img, displacement, axis = (0, 1))
 
 plt.figure()
-plt.imshow(img)
+plt.imshow(X)
 
 plt.figure()
-plt.imshow(img_deformed)
+plt.imshow(X_deformed)
 
 plt.show()
