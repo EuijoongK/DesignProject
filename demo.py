c39e264ddcfbd90015af2e7e583d6d0ba6e73599
EuijoongK <kimeuijoong0602@gmail.com>
2023-10-27 22:32:57 +0900

    10/27

--- demo.py
+++ demo.py
@@ -6,43 +6,43 @@ import cv2
 def gaussian(x, mean, sigma):
     return (1 / np.sqrt(2 * np.pi * sigma ** 2)) * np.exp(-(x - mean) ** 2 / (2 * sigma ** 2))
 
-def deform_field(x, y, x_beg, y_beg, x_end, y_end, strength = 40, coef = 20, sigma = 20):
+def deformation_field(rows, cols, x_beg, x_end, y_beg, y_end, pressure = 30, sigma = 20):
     a = (y_end - y_beg) / (x_end - x_beg)
     b = y_end - a * x_end
     _a = -1 / a
     _b = y_end - _a * x_end
 
-    distance = np.abs(a * x - y + b) / np.sqrt(a ** 2 + 1)
-    amplitude, angle = 0, 0
-
-    if (_a * x + _b) < y:
-        return (amplitude, angle)
-
-    angle = np.arctan((x_beg - x_end) / (y_beg - y_end))
-    amplitude = gaussian(distance, 0, 2) * strength
+    displacement = np.zeros([2, rows, cols])
     
-    return (amplitude, angle)
+    for i in range(rows):
+        for j in range(cols):
+            distance = np.abs(a * j - i + b) / np.sqrt(a ** 2 + 1)
+            amplitude, angle = 0, 0
 
-img = plt.imread("/mnt/c/dl/frame_0.png")
+            if _a * j + _b < i:
+                continue
+
+            angle = np.arctan((x_beg - x_end) / (y_end - y_beg))
+            amplitude = gaussian(distance, 0, 2) * pressure
+            
+            displacement[0][i][j] = -amplitude * np.cos(angle)
+            displacement[1][i][j] = -amplitude * np.sin(angle)
+
+    return displacement
+
+img = plt.imread("/mnt/c/Users/sammy/Desktop/frame_0.png")
 
 if len(img) > 2:
     img = img.mean(axis = 2)
     
 rows, cols = img.shape[0], img.shape[1]
-displacement = np.zeros([2, rows, cols])
 
 x_beg = 200
 y_beg = -1
 x_end = 300
 y_end = 200
 
-for i in range(rows):
-    for j in range(cols):
-        amplitude, angle = deform_field(j, i, x_beg, y_beg,
-                                        x_end, y_end)
-        displacement[0][i][j] = -amplitude * np.cos(angle)
-        displacement[1][i][j] = -amplitude * np.sin(angle)
-    
+displacement = deformation_field(rows, cols, x_beg, x_end, y_beg, y_end)
 img_deformed = elasticdeform.deform_grid(img, displacement,
                                          axis = (0, 1))
 
--- tes6.py
+++
@@ -1,6 +0,0 @@
-import numpy as np
-
-
-def strength(x, y, x_beg y_beg, x_end, y_end, coef, sigma):
-    amplitude = coef * np.exp(-((x - x_beg) ** 2 + (y - y_beg) ** 2) / 2 * sigma ** 2) + coef * np.exp(-((x - x_beg) ** 2 + (y - y_beg) ** 2) / 2 * sigma ** 2)
-    angle = np.arctan((x - x_beg) / (y - y_beg))
