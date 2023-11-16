0acb48593ec1da5b270ddbf4cc2231cd4ef898d2
EuijoongK <kimeuijoong0602@gmail.com>
2023-11-16 18:27:47 +0900

    11/16

--- image_demo.py
+++ image_demo.py
@@ -1,10 +1,14 @@
 from elastic import *
 import cv2
 
-img = cv2.imread("/mnt/c/dl/frame_0.png")
+img = cv2.imread("/mnt/c/dl/frame_0.png", cv2.IMREAD_GRAYSCALE)
+cv2.imshow('test', img)
 
+"""
 if len(img) > 2:
-    img = img.mean(axis = 2)   
+    img = img.mean(axis = 2) 
+    print("Channel reduce")
+"""
 rows, cols = img.shape[0], img.shape[1]
 
 print(img.shape)
@@ -18,10 +22,8 @@ x_end = 250
 y_end = 170
 
 displacement = deformation_field(rows, cols, x_beg, x_end, y_beg, y_end)
-img_deformed = elasticdeform.deform_grid(img, displacement,
-                                         axis = (0, 1))
+img_deformed = elasticdeform.deform_grid(img, displacement)
 
-
-cv2.imshow('Original', img)
+cv2.imshow('test2', img_deformed)
 cv2.waitKey(0)
 cv2.destroyAllWindows()
