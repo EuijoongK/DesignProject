47693ce1abef3841fa647e1eeb5792994bac439e
EuijoongK <kimeuijoong0602@gmail.com>
2023-11-16 20:19:26 +0900

    11/16

--- random_generate.py
+++ random_generate.py
@@ -1,17 +1,18 @@
 from elastic import *
 from gt_generate import *
+import matplotlib.pyplot as plt
 
-img = plt.imread("/mnt/c/dl/frame_0.png")
+img = plt.imread("/mnt/c/Users/Sammy/Desktop/frame_0.png")
 rows, cols = img.shape[0], img.shape[1]
 
 x_beg = int(rows / 2)
 y_beg = -1
 
-needle_angle = (np.random() - 0.5) * np.pi / 2
-needle_length = np.random() * 60
+needle_angle = (np.random.rand() - 0.5) * np.pi / 2
+needle_length = np.random.rand() * 60
 
 x_end = x_beg + needle_length * np.sin(needle_angle)
-y_end = y_end + needle_length * np.cos(needle_angle)
+y_end = y_beg + needle_length * np.cos(needle_angle)
 
 result = trajectory(img, x_beg, x_end, y_beg, y_end)
 result2 = vector_field(img, x_beg, x_end, y_beg, y_end)
