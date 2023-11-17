745c089e3f7f9ccc5a24afe9ae63d90d0c8ac310
EuijoongK <kimeuijoong0602@gmail.com>
2023-11-17 17:12:02 +0900

    11/17

--- random_generate.py
+++ random_generate.py
@@ -6,7 +6,7 @@ img = plt.imread("gray_frame_0.png")
 
 plt.figure()
 plt.imshow(img, cmap = 'gray')
-plt.savefig('original.png')
+#plt.savefig('original.png')
 
 rows, cols = img.shape[0], img.shape[1]
 
@@ -27,16 +27,16 @@ result2 = vector_field(img, x_beg, x_end, y_beg, y_end)
 
 plt.figure()
 plt.imshow(img_deformed, cmap = 'gray')
-plt.savefig("destorted.png")
+#plt.savefig("destorted.png")
 
 plt.figure()
 plt.imshow(result)
-plt.savefig('gt.png')
+#plt.savefig('gt.png')
 
 plt.figure()
 plt.imshow(result2)
-plt.savefig('grid.png')
-#plt.show()
+#plt.savefig('grid.png')
+plt.show()
 
 print("x_beg : {} y_beg : {}".format(x_beg, y_beg))
 print("x_end : {} y_end : {}".format(x_end, y_end))
